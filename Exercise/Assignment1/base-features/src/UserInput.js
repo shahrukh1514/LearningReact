bb8c9f442d802a2fcfd08d1bf9332cc07159d77b
Mohammed Owes <v-owes@cpaperless.com>
2019-07-08 13:53:56 +0530

    created UserInput component for Assignemnt1

---
+++ Exercise/Assignment1/base-features/src/UserInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class UserInput extends React.Component{
+    render()
+    {
+        return (
+            <input type="text" />
+         );
+    }
+}
+
+export default UserInput;
